6b8bfba3192c2fcd21b36cff3ff65bcb91495657
rvs101 <raulvelasalas@gmail.com>
2019-06-24 13:44:21 +0200

    Agregar una One Page para mostrar todos los elementos del curso

--- netbeans/Modulos/public_html/sources/assets/js/scripts.js
+++ netbeans/Modulos/public_html/sources/assets/js/scripts.js
@@ -1,24 +1,9 @@
-/* 
- Created on : 17-jun-2019, 14:18:27
- Author     : Rad
- ###Anotaciones###  
- - outerHeight : Obtenga la altura exterior calculada actual 
- (incluido el relleno, el borde y, opcionalmente, el margen)
- 
- */
-
 /**
- * Función scroll_to 
  * 
- * Maneja la animacion del scroll 'desplazamiento' 
- * al lugar deseado de la pagina
- * 
- * @argument {object} clicked_link
- * @argument {object} nav_height
  * @type Number
  */
 function scroll_to(clicked_link, nav_height) {
-  var element_class = clicked_link.attr('href').replace('#', '.'); // Cambiamos el tipo de identificador
+  var element_class = clicked_link.attr('href').replace('#', '.');
   var scroll_to = 0;
   if (element_class != '.top-content') {
     element_class += '-container';
@@ -26,26 +11,63 @@ function scroll_to(clicked_link, nav_height) {
   }
   if ($(window).scrollTop() != scroll_to) {
     $('html, body').stop().animate(
-            {scrollTop : scroll_to}, 1000);
+            {scrollTop: scroll_to}, 1000);
   }
 }
+
 /**
- * Navigation
  * 
- *  Cada vez que un usuario hace click en un enlace 
- *  con la clase 'scroll-link' llama a esta función.
+ * @type type
  */
-$(document).ready(function () {
+jQuery(document).ready(function () {
+
+  /*
+   Navigation
+   */
   $('a.scroll-link').on('click', function (e) {
-    e.preventDefault(); // detiene la ejecución de este evento pero no del resto
+    e.preventDefault();
     scroll_to($(this), $('nav').outerHeight());
   });
-
-// .top-content = pertenece al elemento de la lista del menu
+  // toggle "navbar-no-bg" class
   $('.top-content .text').waypoint(function () {
-// toggle "navbar-no-bg" class
     $('nav').toggleClass('navbar-no-bg');
   });
+
+  /*
+   Background slideshow
+   */
+  $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
+  $('.call-to-action-container').backstretch("assets/img/backgrounds/1.jpg");
+  $('.testimonials-container').backstretch("assets/img/backgrounds/1.jpg");
+
+  $('#top-navbar-1').on('shown.bs.collapse', function () {
+    $('.top-content').backstretch("resize");
+  });
+  
+  $('#top-navbar-1').on('hidden.bs.collapse', function () {
+    $('.top-content').backstretch("resize");
+  });
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function () {
+    $('.testimonials-container').backstretch("resize");
+  });
+
+  /*
+   Wow
+   */
+  new WOW().init();
+
 });
 
-$('.top-content').backstretch("sources/img/background/grisaceo.jpg");
+/**
+ * 
+ */
+jQuery(window).load(function () {
+
+  /*
+   Hidden images
+   */
+  $(".testimonial-image img").attr("style", "width: auto !important; height: auto !important;");
+
+});
+
